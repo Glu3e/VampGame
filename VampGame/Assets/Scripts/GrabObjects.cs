bce0813dc65b0f77db3711c44df3cb9a248be10e
Kevin <kevincornejoandrade@kevins-MacBook-Pro.local>
2015-09-28 17:14:35 -0400

    PlayerControls
    player “jimmy” moves around and is able to grab objects and move them
    around

---
+++ VampGame/Assets/Scripts/GrabObjects.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GrabObjects : MonoBehaviour {
+
+	GameObject grabbedObject;
+	float grabbedObjectSize;
+
+	GameObject getMouseHoverObject(float range)
+	{
+		Vector3 position = gameObject.transform.position;
+		RaycastHit raycastHit;
+		Vector3 target = position + Camera.main.transform.forward * range; 
+
+		if (Physics.Linecast (position, target, out raycastHit)) 
+			return raycastHit.collider.gameObject;
+			return null;
+	}
+
+	void TryGrabObject(GameObject grabObject) 
+	{
+		if (grabObject == null || !CanGrab(grabObject))
+			return;
+
+		grabbedObject = grabObject;
+		grabbedObjectSize = grabbedObject.GetComponent<Renderer>().bounds.size.magnitude;
+	}
+
+	bool CanGrab(GameObject candidate)
+	{
+		return candidate.GetComponent<Rigidbody> () != null;
+	}
+
+	void DropObject()
+	{
+		if (grabbedObject == null)
+			return;
+		if (grabbedObject.GetComponent<Rigidbody> () != null)
+			grabbedObject.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			grabbedObject = null;
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			if(grabbedObject == null)
+				TryGrabObject(getMouseHoverObject(30));
+			else
+				DropObject();
+		}
+		if (grabbedObject != null) 
+		{
+			Vector3 newPosition = gameObject.transform.position+Camera.main.transform.forward* grabbedObjectSize;
+			grabbedObject.transform.position = newPosition;
+		}
+	
+	}
+}
---
+++ VampGame/Assets/Scripts/OnCollisionCheck.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCollisionCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onColliderEnter(Collision collision)
+	{
+//		foreach (ContactPoint contact in collision.contacts)
+//			Debug.Log ("I COLLIDED");
+		Destroy (collision.gameObject);
+	}
+}
